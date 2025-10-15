1ba826ff26cfdc55811b70c57dd6bbe069415ef9
vbisso <valeriabisso19@gmail.com>
2025-10-14 22:28:11 -0600

    Cross-Component Communication

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -11,17 +11,13 @@ import { ContactService } from '../contact.service';
 export class ContactListComponent implements OnInit {
   contacts: Contact[] = [];
 
-  //event emitter
-  @Output() selectedContactEvent = new EventEmitter<Contact>();
-  onSelected(contact: Contact) {
-    this.selectedContactEvent.emit(contact);
-  }
-
-  constructor(private contactService: ContactService) {
-    //modify the constructor() method to inject the ContactService into the ContactListComponent class.
-  }
+  constructor(private contactService: ContactService) {}
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
+  }
+
+  onSelected(contact: Contact) {
+    this.contactService.contactSelectedEvent.emit(contact);
   }
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -9,4 +10,12 @@ import { Contact } from './contact.model';
 })
 export class ContactsComponent {
   selectedContact: Contact | null = null; //holds the clicked contact to later pass it down to contact-detail component
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contactService.contactSelectedEvent.subscribe((contact: Contact) => {
+      this.selectedContact = contact;
+    });
+  }
 }
